51a751455e964f32fbc0007dfab9436b8e6725a0
Peter NDENGO <frdrcpeter@gmail.com>
2023-01-07 20:47:27 +0100

    Change text

--- components/atoms/Description.js
+++ components/atoms/Description.js
@@ -1,5 +1,5 @@
 const Description = () => {
-    return <p className="text-gray text-sm px-12">Trouvez votre prochain kou par des milliers d’hommes et femmes.</p>;
+    return <p className="text-gray text-sm px-8">Trouvez votre prochain kou par des milliers d’hommes et femmes.</p>;
 }
 
 export default Description;
